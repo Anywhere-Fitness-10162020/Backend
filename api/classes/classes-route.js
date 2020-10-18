d703bf95c927f777b7a7c0346ec032fa9fc0fc14
git-austinmccollom <austinmccollom@gmail.com>
2020-10-18 00:35:29 -0500

    classes create and read endpoints

--- api/classes/classes-route.js
+++ api/classes/classes-route.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const server = require("../server");
 const dbFun = require("./classes-model");
 
 const router = express.Router();
 
+//CRUD
+//Create
+router.post('/', (req, res) => {
+    dbFun.createClass(req.body)
+    .then((dbRes) => {
+        res.status(201).json(dbRes);
+      })
+      .catch((dbErr) => {
+        res.status(500).json(dbErr);
+      });
+})
+
+//Read
+router.get('/', (req, res) => {
+    dbFun.getClasses()
+    .then((dbRes) => {
+        res.status(200).json(dbRes);
+      })
+      .catch((dbErr) => {
+        res.status(500).json(dbErr);
+      });
+})
+//Update
+//Delete
 module.exports = router;
