77cb57f7d9c7bf305e2e5399e53060396fb66a64
git-austinmccollom <austinmccollom@gmail.com>
2020-10-22 12:29:30 -0500

    resetPunches

--- api/punches/punches-model.js
+++ api/punches/punches-model.js
@@ -6,16 +6,19 @@ module.exports = {
 }
 
 function getPunches() {
+    console.log("getPunches()")
     return db('punches')
 }
 
 async function getPunchById(incomingPunchObj) {
+    console.log("getPunchById()")
     const { pass_id, user_id } = incomingPunchObj;
     const punch = await db('punches').where({ pass_id, user_id });
     return punch
 }
 
 async function checkExisting(incomingPunchObj) {
+    console.log("checkExisting()")
     const { pass_id, user_id } = incomingPunchObj;
     const existingPunch = await db('punches').where({ pass_id, user_id });
     // console.log(existingPunch)
@@ -28,8 +31,10 @@ async function checkExisting(incomingPunchObj) {
 }
 
 async function checkIfPunchCardFull(incomingPunchObj) {
+    console.log("checkIfPunchCardFull()")
     const { pass_id, user_id } = incomingPunchObj;
     const passRecord = await db('passes').select('max_punches').where('id', pass_id)
+    console.log('passRecord', passRecord)
     const maxPunches = passRecord[0].max_punches
     console.log(maxPunches)
 
@@ -48,11 +53,13 @@ async function checkIfPunchCardFull(incomingPunchObj) {
 }
 
 async function setFreeClassToTrue(incomingPunchObj) {
+    console.log("setFreeClassToTrue()")
     const { pass_id, user_id } = incomingPunchObj;
     
     const oldCard = await getPunchById({ pass_id, user_id })
     const newCard = {...oldCard[0]}
     newCard.free_class = 1;
+    console.log('newCard', newCard)
     
     return db('punches').where({ pass_id, user_id }).update(newCard)
     .then( updateRes => {
@@ -64,6 +71,7 @@ async function setFreeClassToTrue(incomingPunchObj) {
 }
 
 function createPunch(incomingPunchObj) {
+    console.log("createPunch()")
     const newPunchObj = {...incomingPunchObj, punch_count: 1}
     return db('punches').insert(newPunchObj)
     .then( createRes => {
@@ -74,11 +82,19 @@ function createPunch(incomingPunchObj) {
     })
 }
 
-function resetPunches() {
-    return "reset punches"
+function resetPunches(incomingPunchObj) {
+    console.log("resetPunches()")
+
+    const { pass_id, user_id } = incomingPunchObj;
+
+    const resetPunch = {...incomingPunchObj, punch_count: 0, free_class: 0}
+
+    return db('punches').where({ pass_id, user_id }).update(resetPunch)
 }
 
 function incrementPunches(existingPunch) {
+    console.log("incrementPunches()")
+
     const pass_id = existingPunch.pass_id;
     const user_id = existingPunch.user_id;
 
@@ -110,10 +126,12 @@ function incrementPunches(existingPunch) {
 }
 
 function decrementPunches () {
+    console.log("decrementPunches()")
     return "decrement punches"
 }
 
 async function punch(incomingPunchObj) {
+    console.log("punch()")
 
     const existingPunchArray = await checkExisting(incomingPunchObj);
     const existingPunch = existingPunchArray[0];
@@ -121,7 +139,14 @@ async function punch(incomingPunchObj) {
     console.log(`existing punch from punch() function`, existingPunch)
     if (existingPunch) {
         if (existingPunch.free_class === 1) {
-            resetPunches();
+            resetPunches(incomingPunchObj)
+            .then( resetRes => {
+                console.log('reset success', resetRes)
+            })
+            .catch( resetErr => {
+                console.log('reset failed', resetErr)
+            })
+            return { free_class: 'true', message: "this is the user's free class, and their punch card has been reset"}
         } else {
             incrementPunches(existingPunch)
             return getPunchById(incomingPunchObj)
